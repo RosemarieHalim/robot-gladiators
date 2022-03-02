09cdd36a411e254d7a2d3710f42da50bd7cdaf68
Rosemarie <rosemarie.m.halim@gmail.com>
2022-03-01 21:29:55 -0500

    start and ned game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,9 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
@@ -70,16 +73,50 @@ var fight = function(enemyName) {
 };
 
 // run fight function to start game
-for(var i = 0; i < enemyNames.length; i++) {
+var startGame = function () {
+  //reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+  }
+  // after loop ends, player is either out of health or has no enemies to fight
+  endGame();
+};
+
+// function to end the entire game
+var endGame = function() {
+  // if player is still alive, player wins!
   if (playerHealth > 0) {
-    window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
+    window.alert ("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
   }
   else {
-    window.alert("You have lost your robot in battle! Game Over!");
-    break;
+    window.alert ("You've lost your robot in battle.")
   }
 
-}
+    // ask player if they want to play again
+  var PlayAgainConfirm = window.confirm ("Would you like to play again?")
+
+  if (PlayAgainConfirm) {
+    // restart game
+    startGame();
+  }
+  else {
+    window.alert ("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+};
+
+
+// start the game when the page loads
+startGame();
